422468456601b344c663b1ba1319b94a03f49942
Luis Angel <santosangel748@gmail.com>
2021-02-17 12:00:28 -0600

    Creando las tablas, entidades y datos de prueba

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        App\User::create([
+            'name' => 'Angel',
+            'email' => 'angel@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
